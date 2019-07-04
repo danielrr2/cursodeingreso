3af2edd32956e318c36eccc2340d5700c0563c77
danielrr2 <52512211+danielrr2@users.noreply.github.com>
2019-07-03 21:52:18 -0300

    Clase 1 avace

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,10 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{var nombre;
+	nombre=prompt("Ingrese su nombre");
+	document.getElementById("elNombre").value=nombre
+	
 
 }
 
